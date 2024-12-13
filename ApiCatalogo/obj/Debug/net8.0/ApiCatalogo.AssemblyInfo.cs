e685ca86e4d455cf9babd9c33bce34206abb69a9
horley.monteiro <horley.monteiro@trixti.com.br>
2024-12-13 09:55:41 -0300

    att

--- ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
+++ ApiCatalogo/obj/Debug/net8.0/ApiCatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1813e2ccfb7fdd8cfb5db4a4725d678c03d3967d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75dfc041b197f9964c31ffb1fcd969f2fad178a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
